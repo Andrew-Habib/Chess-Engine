d4f1bb73cbddf767b1f38a563bf15f0c3b1024be
Andrew Habib <andrew9ca@gmail.com>
2024-05-07 20:14:43 -0400

    Add project files.

---
+++ Assets/src/BoardGenerator.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class InterfaceInitializer : MonoBehaviour {
+
+    public Sprite tileSprite;
+
+    // Start is called before the first frame update
+    void Start(){
+        generateBoard();
+    }
+
+    void generateBoard() {
+        bool isWhite = false;
+
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+
+                GameObject tile = new GameObject("Tile_" + row + "_" + col);
+
+                SpriteRenderer renderer = tile.AddComponent<SpriteRenderer>();
+                renderer.sprite = tileSprite;
+
+                renderer.color = isWhite ? Color.white : Color.black;
+                tile.transform.localPosition = new Vector2(-4f + row, -4f + col);
+
+                isWhite = !isWhite;
+
+            }
+            isWhite = !isWhite;
+        }
+    }
+
+}
