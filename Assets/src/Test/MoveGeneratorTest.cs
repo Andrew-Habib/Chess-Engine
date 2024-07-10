5a13589a5ae192cfaa9e84741f77c443a3ee7bd8
Andrew Habib <andrew9ca@gmail.com>
2024-07-09 21:29:47 -0400

    Fix Errors caused by NUnit experimentation

--- Assets/src/Test/MoveGeneratorTest.cs
+++ Assets/src/Test/MoveGeneratorTest.cs
@@ -1,6 +1,38 @@
-﻿using NUnit.Framework;
+﻿//using NUnit.Framework;
+//using System.Collections.Generic;
 
-namespace Assets.src {
-    internal class MoveGeneratorTest {
-    }
-}
+//// Perform a test to check if number of moves generated correlates to Shannon's calculation
+
+//namespace Assets.src {
+//    [TestFixture]
+//    class MoveGeneratorTest {
+
+//        private Board board;
+//        private List<int[]> moves;
+
+//        [SetUp]
+//        public void SetUp() {
+//            board = new();
+//            board.initChessBoard();
+//        }
+
+//        [Test]
+//        public void positionsOneHalfMove() {
+//            moves = MoveGenerator.allPlayerMoves(board.getChessPieces(), true, false);
+//            Assert.Equals(2, 3);
+//        }
+
+//        [Test]
+//        public void positionsTwoHalfMoves() {
+//            //for (int r = 0; r < 8; r++) {
+//            //    for (int c = 0; c < 8; c++) {
+//            //        List<int[]> firstMoves = MoveGenerator.generateMovesAbstract(board.getChessPieces(), r, c, true, false);
+//            //        foreach (int[] move in firstMoves) {
+//            //            break;  
+//            //        }
+//            //    }
+//            //}
+//        }
+
+//    }
+//}
