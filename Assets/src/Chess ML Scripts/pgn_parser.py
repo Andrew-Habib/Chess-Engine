e3831c4e29fbd4c40c7efd6898d8eb0d9d931877
Andrew Habib <andrew9ca@gmail.com>
2024-07-12 20:47:16 -0400

    Completed pgn parsing and added data from elite database to new MongoDB

--- Assets/src/Chess ML Scripts/pgn_parser.py
+++ Assets/src/Chess ML Scripts/pgn_parser.py
@@ -1,17 +1,36 @@
 ﻿import chess.pgn as pgnLibrary
 import pymongo
 
-def png_to_mongo(filename, games):
-    with open(filename) as file:
-        game = ""
-        while game is not None:
-            game = pgnLibrary.read_game(file)
-            print(game)
+def pgn_to_mongo(filenames, games):
+    for filename in filenames:
+        with open(filename) as file:
+            while True:
+                # Read each game in the pgn file
+                game = pgnLibrary.read_game(file)
+                if game is None:
+                    break
+                # Extract the first 8 moves (or less) from each game
+                num_moves = 0
+                moves = []
+                for move in game.mainline_moves():
+                    moves.append(str(move))
+                    num_moves = num_moves + 1
+                    if num_moves == 8:
+                        break
+                # Insert each game into MongoDB
+                games.insert_one({
+                    "game_id": game.headers["LichessURL"],
+                    "white": game.headers["White"],
+                    "black": game.headers["Black"],
+                    "moves": moves,
+                    "result": game.headers["Result"]
+                })
 
-filename = 'lichess_elite_2022-03.pgn'
+filenames = ['lichess_elite_2022-03.pgn']
 
-client = pymongo.MongoClient('localhost:27017')
-db = client["lichess_db"]
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+client.drop_database("elite_lichess_openings_db")
+db = client["elite_lichess_openings_db"]
 games = db["games"]
 
-png_to_mongo(filename=filename, games=games)
+pgn_to_mongo(filenames=filenames, games=games)
