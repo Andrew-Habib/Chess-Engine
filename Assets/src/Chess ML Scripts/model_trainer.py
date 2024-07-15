69952a8243b618ae6549b07d515272826e23ea16
Andrew Habib <andrew9ca@gmail.com>
2024-07-14 22:52:04 -0400

    Completed the structure of the Neural Network deep learning model using Tensorflow tools (Issue with data as strings)

--- Assets/src/Chess ML Scripts/model_trainer.py
+++ Assets/src/Chess ML Scripts/model_trainer.py
@@ -1,16 +1,13 @@
 ﻿import pymongo
-import numpy as np
+import numpy
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
-
 
 def extract_mongodb_chess_data():
     client = pymongo.MongoClient('mongodb://localhost:27017/')
     db = client["elite_lichess_openings_db"]
     games = db["games"]
     return list(games.find({}))
-
-
 
 def get_input_output_model(chess_data):
     input_moves = []
@@ -22,7 +19,27 @@ def get_input_output_model(chess_data):
             output_moves.append(game_moves[i])
     return input_moves, output_moves
 
+def get_num_unique_moves(chess_data):
+    np_chess_data = numpy.array(chess_data)
+    unique_moves = set()
+    for data in np_chess_data:
+        unique_moves.update(data)
+    return len(unique_moves)
+
 chess_data = extract_mongodb_chess_data()
 X, y = get_input_output_model(chess_data=chess_data)
 
-print(X, y)
+num_unique_moves = get_num_unique_moves(chess_data=chess_data)
+embedding_dim = 100
+num_epochs = 10
+
+model = Sequential() # Defining the neural network (Deep learning model)
+model.add(Embedding(input_dim=num_unique_moves, output_dim=embedding_dim))
+model.add(LSTM(units=128))
+model.add(Dense(units=num_unique_moves, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X, y, epochs=num_epochs, validation_split=0.2)
+
+model.save('chess_ai_model.h5')
