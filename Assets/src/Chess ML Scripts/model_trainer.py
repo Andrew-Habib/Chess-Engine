213398f3c149e2ff24dae34384c5b1065597b2a5
Andrew Habib <andrew9ca@gmail.com>
2024-07-15 22:23:02 -0400

    Completed neural network that predicts optimal follow up moves in the opening using large high level gameplay (Need to run and edit)

--- Assets/src/Chess ML Scripts/model_trainer.py
+++ Assets/src/Chess ML Scripts/model_trainer.py
@@ -1,7 +1,7 @@
-﻿import pymongo
-import numpy
+﻿import pymongo, numpy
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 def extract_mongodb_chess_data():
     client = pymongo.MongoClient('mongodb://localhost:27017/')
@@ -14,29 +14,40 @@ def get_input_output_model(chess_data):
     output_moves = []
     for game in chess_data:
         game_moves = game['moves']
-        for i in range(0, len(game_moves)):
+        for i in range(1, len(game_moves)):
             input_moves.append(game_moves[:i])
             output_moves.append(game_moves[i])
     return input_moves, output_moves
 
-def get_num_unique_moves(chess_data):
-    np_chess_data = numpy.array(chess_data)
+def get_unique_moves(chess_data):
     unique_moves = set()
-    for data in np_chess_data:
-        unique_moves.update(data)
-    return len(unique_moves)
+    for game in chess_data:
+        unique_moves.update(game['moves'])
+    return list(unique_moves)
+
+def encode_moves(moves, move_encoder):
+    return [move_encoder[move] for move in moves]
+
 
 chess_data = extract_mongodb_chess_data()
-X, y = get_input_output_model(chess_data=chess_data)
+input_moves, output_moves = get_input_output_model(chess_data)
+unique_moves = get_unique_moves(chess_data)
 
-num_unique_moves = get_num_unique_moves(chess_data=chess_data)
+move_encoder = {move: i for i, move in enumerate(unique_moves)}
+encoded_input_moves = [encode_moves(moves, move_encoder) for moves in input_moves]
+encoded_output_moves = encode_moves(output_moves, move_encoder)
+
+max_sequence_length = max(len(moves) for moves in encoded_input_moves)
 embedding_dim = 100
 num_epochs = 10
 
-model = Sequential() # Defining the neural network (Deep learning model)
-model.add(Embedding(input_dim=num_unique_moves, output_dim=embedding_dim))
+X = pad_sequences(encoded_input_moves, maxlen=max_sequence_length)
+y = numpy.array(encoded_output_moves)
+
+model = Sequential()
+model.add(Embedding(input_dim=len(unique_moves), output_dim=embedding_dim, input_length=max_sequence_length))
 model.add(LSTM(units=128))
-model.add(Dense(units=num_unique_moves, activation='softmax'))
+model.add(Dense(units=len(unique_moves), activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
