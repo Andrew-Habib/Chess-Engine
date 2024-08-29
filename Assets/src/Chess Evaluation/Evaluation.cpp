cbb4121363feee63885d91bd5cf7acd453eb266f
Andrew-Habib <andrew9ca@gmail.com>
2024-08-29 00:30:59 -0400

    Worked on base evaluation criteria for chess engine like material imbalance and king safety

--- Assets/src/Chess Evaluation/Evaluation.cpp
+++ Assets/src/Chess Evaluation/Evaluation.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // Base researched criteria (Material, King Safety, Space, Mobility (could be similar to space), Piece Coordination, Pawn structure)
-// Personal additions: Forced combination advantage (search will do this), Piece positioning
+// Personal additions: Forced combination advantage (search will do this), Piece positioning, passed pawns
 // Will use principle evaluation ideas + extensions/customizations but with unique methods and weights
 // Source: https://chessify.me/blog/chess-engine-evaluation
 
@@ -12,17 +12,34 @@ using namespace std;
 #include <sstream>
 using namespace std;
 
-void getDepth3Positions();
+vector<vector<vector<int>>> getDepth3Positions();
+float evaluatePos(vector<vector<int>> position);
+float materialScore(vector<vector<int>> position);
+float kingScore(vector<vector<int>> position);
+float mobilityScore(vector<vector<int>> position);
+float coordScore(vector<vector<int>> position);
+float piecePosScore(vector<vector<int>> position);
 
 int main() {
-    getDepth3Positions();
+    vector<vector<vector<int>>> d3Positions = getDepth3Positions();
+    for (const auto& position : d3Positions) {
+        for (const auto& row : position) {
+            for (int value : row) {
+                cout << value << " ";
+            }
+            cout << endl;
+        }
+        float evaluation = evaluatePos(position);
+        cout << "Eval: " << evaluation << " \n";
+    }
+    
     return 0;
 }
 
-void getDepth3Positions() { 
+vector<vector<vector<int>>> getDepth3Positions() { 
     ifstream file("../../../data.txt");
     string line;
-    vector<vector<vector<int>>> d3Positions; // 3D vector to store arrays
+    vector<vector<vector<int>>> d3Positions;
     vector<vector<int>> currentArray;
     vector<int> currentRow;
 
@@ -32,11 +49,10 @@ void getDepth3Positions() {
 
     while (getline(file, line)) {
         if (line.empty()) {
-            continue; // Skip empty lines
+            continue;
         }
 
         if (line == "|") {
-            // End of a 2D array
             if (!currentArray.empty()) {
                 d3Positions.push_back(currentArray);
                 currentArray.clear();
@@ -63,47 +79,77 @@ void getDepth3Positions() {
         }
     }
 
-    // Add the last array if needed
     if (!currentArray.empty()) {
         d3Positions.push_back(currentArray);
     }
 
     file.close();
 
-    // Print the d3Positions
-    for (const auto& matrix : d3Positions) {
-        for (const auto& row : matrix) {
-            for (int value : row) {
-                cout << value << " ";
-            }
-            cout << endl;
+    return d3Positions;
+
+}
+
+float evaluatePos(vector<vector<int>> position) {
+
+    float position_score = 0;
+
+    float material_score = materialScore(position);
+    float king_score = kingScore(position);
+    float mobility_score = mobilityScore(position);
+    float coord_score = coordScore(position);
+    float piece_pos_score = piecePosScore(position);
+
+    position_score = material_score + king_score + mobility_score + coord_score + piece_pos_score;
+
+    return position_score;
+
+}
+
+float materialScore(vector<vector<int>> position) {
+
+    float score = 0;
+    for (const auto& row : position) {
+        for (int piece : row) {
+            score = score + piece;
         }
-        cout << endl;
     }
+    return score;
 
 }
 
-int evaluate_pos() {
-    int move_score = 0;
+float kingScore(vector<vector<int>> position) {
+    float score = 0;
+    vector<vector<float>> preferred_king_squares = { 
+        {1, 1, 0.75, 0.5, 0.5, 0.75, 1, 1},
+        {0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {-0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25, -0.25},
+        {-1, -1, -0.75, -0.5, -0.5, -0.75, -1, -1}
+    };
+
+    for (int i = 0; i < position.size(); i++) {
+        for (int j = 0; j < position[i].size(); j++) {
+            if (position[i][j] == 1000 || position[i][j] == -1000) {
+                score = score + preferred_king_squares[i][j];
+            }
+        }
+    } // Check pieces around the king
+    // https://www.netlib.org/utk/lsi/pcwLSI/text/node343.html#:~:text=King%20safety%20is%20evaluated%20by,in%20front%20of%20the%20king.
+
+    return score;
+}
+
+float mobilityScore(vector<vector<int>> position) {
     return 0;
 }
 
-int material_score() {
+float coordScore(vector<vector<int>> position) {
     return 0;
 }
 
-int king_score() {
-    return 0;
-}
-
-int space_score() {
-    return 0;
-}
-
-int coord_score() {
-    return 0;
-}
-
-int piece_pos_score() {
+float piecePosScore(vector<vector<int>> position) {
     return 0;
 }
